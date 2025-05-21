22e1517634ce4bef097f759b9aa9ead048c97cde
IsmaEstanis <ismael.estanislao@badak.com.mx>
2025-05-20 22:31:52 -0600

    feat(generateCities): Add a metod to calculates distance between cities and return as the DTO demands

--- src/tsp/tsp.service.ts
+++ src/tsp/tsp.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, NotImplementedException } from '@nestjs/common';
 import { TspSolveResponseDto } from './dtos/response/solve.response.dto';
 import { TspSolveRequestDto } from './dtos/request/solve.request.dto';
-import { TspGenerateCitiesResponseDto } from './dtos/response/generate-cities.response.dto';
+import { TspDistanceResponseDto, TspGenerateCitiesResponseDto } from './dtos/response/generate-cities.response.dto';
 import { WorldGenerator } from './domain/world-generator/world-generator';
 import { TspGenerateCitiesRequestDto } from './dtos/request/generate-cities.request.dto';
+import { City } from './domain/world-generator/city';
 
 /**
  * The TspService class is a NestJS service responsible for implementing the
@@ -34,9 +35,37 @@ export class TspService {
 
         // To do
         // - Calculate distance between cities
-
+        /*
         throw new NotImplementedException(
             `${this.generateCities.name} method not implemented in ${TspService.name}`,
         );
+        */
+       return this.calculateDistances(worldGenerator.getWorld().cities);
+    }
+
+    private calculateDistances(cities: City[]): TspGenerateCitiesResponseDto {
+        const citiesNames = cities.map(city => city.name);
+        let distances: TspDistanceResponseDto[] = [];
+
+        for (let i = 0; i < cities.length; i++) {
+            const { name: fromName, coordinates: fromCoord } = cities[i];
+    
+            for (let j = i + 1; j < cities.length; j++) {
+    
+                const { name: toName, coordinates: toCoord } = cities[j];
+    
+                const dx = fromCoord.x - toCoord.x;
+                const dy = fromCoord.y - toCoord.y;
+                const distanceValue = Math.hypot(dx, dy);
+    
+                distances.push({
+                    from: fromName,
+                    to: toName,
+                    distance: distanceValue
+                });
+            }
+        }
+
+        return {cities: citiesNames, distances: distances};
     }
 }
